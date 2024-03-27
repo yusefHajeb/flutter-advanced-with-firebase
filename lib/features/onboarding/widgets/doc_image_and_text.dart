f836e354c420222b410c97b9709269a31687c8fd
yousefRafal <146264504+yousefRafal@users.noreply.github.com>
2024-03-28 02:07:21 +0300

    added doctor image and text on onboarding screen

---
+++ lib/features/onboarding/widgets/doc_image_and_text.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_advanced_with_firebase/core/helper/extentions.dart';
+import 'package:flutter_advanced_with_firebase/core/theme/app_colors.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+class DoctorImageAndText extends StatelessWidget {
+  const DoctorImageAndText({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: [
+        SvgPicture.asset("assets/svgs/docdoc_logo_low_opacity.svg"),
+        Container(
+          foregroundDecoration: BoxDecoration(
+            gradient: LinearGradient(
+              tileMode: TileMode.mirror,
+              colors: [
+                Colors.white,
+                Colors.white.withOpacity(0.0),
+              ],
+              begin: Alignment.bottomCenter,
+              end: Alignment.topCenter,
+              stops: const [0.14, 0.4],
+            ),
+          ),
+          child: Image.asset('assets/images/onboarding_doctor.png'),
+        ),
+        Positioned(
+          bottom: 30,
+          left: 0,
+          right: 0,
+          child: Text(
+            'Best Doctor\nAppointment App',
+            style:
+                context.theme.textTheme.bodyLarge?.copyWith(color: Colors.blue),
+            textAlign: TextAlign.center,
+          ),
+        ),
+      ],
+    );
+  }
+}
