b80416b547ab79cb793a1ef85103d089e148dc50
yousefRafal <146264504+yousefRafal@users.noreply.github.com>
2024-03-28 02:04:24 +0300

    added appSize class

---
+++ lib/core/helper/app_size.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+class AppSize {
+  static SizedBox height5 = SizedBox(height: 5.sp);
+  static SizedBox height10 = SizedBox(height: 10.sp);
+  static SizedBox height20 = SizedBox(height: 20.sp);
+  static SizedBox height30 = SizedBox(height: 30.sp);
+  static SizedBox height40 = SizedBox(height: 40.sp);
+  static SizedBox widht5 = SizedBox(width: 5.sp);
+  static SizedBox width10 = SizedBox(width: 10.sp);
+}
