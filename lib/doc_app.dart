f6151e9e2be137d33e41efadad83e43d46ad953d
yousefRafal <146264504+yousefRafal@users.noreply.github.com>
2024-03-28 02:06:29 +0300

    added initilize app

---
+++ lib/doc_app.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_advanced_with_firebase/core/routes/app_router.dart';
+import 'package:flutter_advanced_with_firebase/core/routes/routes.dart';
+import 'package:flutter_advanced_with_firebase/core/theme/app_theme.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+// ignore: must_be_immutable
+class DocApp extends StatelessWidget {
+  final AppRouter appRouter;
+  DocApp({super.key, required this.appRouter});
+
+  // ThemeData themeData = AppTheme.light;
+  @override
+  Widget build(BuildContext context) {
+    ScreenUtil.init(context, designSize: Size(375, 689));
+    return ScreenUtilInit(
+      // designSize: const Size(375, 690),
+      minTextAdapt: false,
+      splitScreenMode: true,
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        // title: "house",
+        theme: AppTheme.light,
+        darkTheme: AppTheme.dark,
+        themeMode: ThemeMode.system,
+        onGenerateRoute: appRouter.genratedRoute,
+        initialRoute: Routes.onBoardingScreen,
+      ),
+    );
+  }
+}
