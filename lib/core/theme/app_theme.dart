404d2255ed4ea92b2334c111144f815dbcce36f6
yousefRafal <146264504+yousefRafal@users.noreply.github.com>
2024-03-28 02:15:05 +0300

    fix gradle

--- lib/core/theme/app_theme.dart
+++ lib/core/theme/app_theme.dart
@@ -1,7 +1,7 @@
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 import '../helper/app_colors_extentions.dart';
-import 'app_typography.dart';
+import 'app_styles.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -6,14 +6,14 @@
 // tree, read text, and verify that the values of widget properties are correct.
 
 import 'package:flutter/material.dart';
+import 'package:flutter_advanced_with_firebase/core/routes/app_router.dart';
+import 'package:flutter_advanced_with_firebase/doc_app.dart';
 import 'package:flutter_test/flutter_test.dart';
-
-import 'package:flutter_advanced_with_firebase/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(DocApp(appRouter: AppRouter()));
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
