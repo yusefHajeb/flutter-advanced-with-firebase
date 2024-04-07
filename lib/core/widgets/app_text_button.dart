f0db8c0c23e392a0fca282ca807df3a05495bb24
yousefRafal <146264504+yousefRafal@users.noreply.github.com>
2024-04-07 03:07:00 +0300

    added appTextButton widget to login widget

---
+++ lib/core/widgets/app_text_button.dart
@@ -0,0 +1,60 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+import 'package:flutter_advanced_with_firebase/core/theme/app_colors.dart';
+
+// ignore: must_be_immutable
+class AppTextButton extends StatelessWidget {
+  final double? borderRadius;
+  final Color? backgroundColor;
+  final double? horizontalPadding;
+  final double? verticalPadding;
+  final double? buttonWidth;
+  final double? buttonHeight;
+  final String buttonText;
+  TextStyle? textStyle;
+  final VoidCallback onPressed;
+  AppTextButton({
+    Key? key,
+    this.borderRadius,
+    this.backgroundColor,
+    this.horizontalPadding,
+    this.verticalPadding,
+    this.buttonWidth,
+    this.buttonHeight,
+    required this.buttonText,
+    this.textStyle,
+    required this.onPressed,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return TextButton(
+      style: ButtonStyle(
+        shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+          RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(borderRadius ?? 16.0),
+          ),
+        ),
+        backgroundColor: MaterialStatePropertyAll(
+          backgroundColor ?? AppColors.primaryColor,
+        ),
+        padding: MaterialStateProperty.all<EdgeInsets>(
+          EdgeInsets.symmetric(
+            horizontal: horizontalPadding?.w ?? 12.w,
+            vertical: verticalPadding?.h ?? 14.h,
+          ),
+        ),
+        fixedSize: MaterialStateProperty.all(
+          Size(buttonWidth?.w ?? double.maxFinite, buttonHeight ?? 50.h),
+        ),
+      ),
+      onPressed: onPressed,
+      child: Text(
+        buttonText,
+        style: textStyle,
+      ),
+    );
+  }
+}
