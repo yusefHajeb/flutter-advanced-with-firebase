432a0a6801b2217d15d9eb5a009cb66ef96f9192
yousefRafal <146264504+yousefRafal@users.noreply.github.com>
2024-04-07 03:09:33 +0300

    add build runner and git it package

--- lib/main.dart
+++ lib/main.dart
@@ -5,14 +5,16 @@ import 'package:flutter_advanced_with_firebase/doc_app.dart';
 import 'package:flutter_native_splash/flutter_native_splash.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
+import 'core/di/dependency_injection.dart';
+
 void main() async {
+  setupGtIt();
   await ScreenUtil.ensureScreenSize();
   WidgetsBinding widgetsFlutterBinding =
       WidgetsFlutterBinding.ensureInitialized();
 
   FlutterNativeSplash.preserve(widgetsBinding: widgetsFlutterBinding);
   FlutterNativeSplash.remove();
-
   await AppTheme.initialize();
   runApp(DocApp(
     appRouter: AppRouter(),
