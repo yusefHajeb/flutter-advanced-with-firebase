e27caf4472744b8e71975b75930b5a879fb3994c
yousefRafal <146264504+yousefRafal@users.noreply.github.com>
2024-04-07 03:08:56 +0300

    restyle login screen

--- lib/features/login/ui/login_screen.dart
+++ lib/features/login/ui/login_screen.dart
@@ -1,4 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_advanced_with_firebase/core/helper/app_size.dart';
+import 'package:flutter_advanced_with_firebase/core/helper/extentions.dart';
+import 'package:flutter_advanced_with_firebase/core/theme/app_colors.dart';
+import 'package:flutter_advanced_with_firebase/core/theme/font_wight_helper.dart';
+import 'package:flutter_advanced_with_firebase/core/widgets/app_text_button.dart';
+import 'package:flutter_advanced_with_firebase/features/login/ui/widgets/email_and_password.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class LoginScreen extends StatelessWidget {
   static const String routeName = 'loginScreen';
@@ -7,8 +14,45 @@ class LoginScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('Login Screen')),
-      body: const Center(child: Text('Login Screen')),
+      body: SafeArea(
+        child: Padding(
+          padding: EdgeInsets.symmetric(horizontal: 30.w, vertical: 30.h),
+          child: SingleChildScrollView(
+            child: Column(
+              children: [
+                Text(
+                  'Wellcome Back',
+                  style: context.theme.textTheme.bodyLarge
+                      ?.copyWith(color: AppColors.primaryColor),
+                ),
+                AppSize.verticalSize(8),
+                Text(
+                    " 'We\'re excited to have you back, can\'t wait to see what you\'ve been up to since you last logged in.",
+                    style: context.theme.textTheme.headlineMedium
+                        ?.copyWith(color: AppColors.lightGray)),
+                AppSize.verticalSize(36),
+                const EmailAndPassword(),
+                AppSize.verticalSize(36),
+                Align(
+                  alignment: AlignmentDirectional.centerEnd,
+                  child: Text(
+                    'Forgot Password?',
+                    style: const TextStyle(
+                        fontSize: 14,
+                        color: AppColors.primaryColor,
+                        fontWeight: FontWeightHelper.extraBold),
+                  ),
+                ),
+                AppSize.verticalSize(36),
+                AppTextButton(
+                    buttonText: 'Login',
+                    textStyle: context.theme.textTheme.bodyMedium,
+                    onPressed: () {})
+              ],
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
---
+++ lib/features/login/ui/widgets/email_and_password.dart
@@ -0,0 +1,103 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:flutter_advanced_with_firebase/features/login/logic/cubit/login_cubet_cubit.dart';
+import 'package:flutter_advanced_with_firebase/features/login/ui/widgets/password_validator.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import '../../../../core/helper/app_regex.dart';
+import '../../../../core/helper/app_size.dart';
+import '../../../../core/widgets/app_text_form_field.dart';
+
+class EmailAndPassword extends StatefulWidget {
+  const EmailAndPassword({super.key});
+
+  @override
+  State<EmailAndPassword> createState() => _EmailAndPasswordState();
+}
+
+class _EmailAndPasswordState extends State<EmailAndPassword> {
+  bool isObscureText = true;
+
+  bool hasLowercase = false;
+  bool hasUppercase = false;
+  bool hasSpecialCharacters = false;
+  bool hasNumber = false;
+  bool hasMinLength = false;
+
+  TextEditingController passwordController = TextEditingController();
+
+  @override
+  void initState() {
+    passwordController = context.read<LoginCubet>().passwordController;
+    super.initState();
+  }
+
+  void setupPasswordControllerListener() {
+    passwordController.addListener(() {
+      setState(() {
+        hasLowercase = AppRegex.hasLowerCase(passwordController.text);
+        hasUppercase = AppRegex.hasUpperCase(passwordController.text);
+        hasSpecialCharacters =
+            AppRegex.hasSpecialCharacter(passwordController.text);
+        hasNumber = AppRegex.hasNumber(passwordController.text);
+        hasMinLength = AppRegex.hasMinLength(passwordController.text);
+      });
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Form(
+      key: context.read<LoginCubet>().formKey,
+      child: Column(
+        children: [
+          AppTextFormField(
+              controller: context.read<LoginCubet>().emailController,
+              hintText: 'email',
+              validator: (value) {
+                if (value == null ||
+                    value.isEmpty ||
+                    !AppRegex.isEmailValid(value)) {
+                  return 'Please enter a valid email';
+                }
+              }),
+          AppSize.verticalSize(18),
+          AppTextFormField(
+              controller: context.read<LoginCubet>().passwordController,
+              hintText: 'password',
+              isObscureText: isObscureText,
+              suffixIcon: GestureDetector(
+                onTap: () {
+                  setState(() {
+                    isObscureText = !isObscureText;
+                  });
+                },
+                child: Icon(
+                  isObscureText ? Icons.visibility_off : Icons.visibility,
+                ),
+              ),
+              validator: (value) {
+                if (value == null || value.isEmpty) {
+                  return 'Please enter a valid password';
+                }
+              }),
+          AppSize.verticalSize(24),
+          const PasswordValidator(
+            hasLowerCase: true,
+            hasUpperCase: false,
+            hasSpecialCharacter: false,
+            hasNumber: false,
+            hasMiningth: false,
+          ),
+        ],
+      ),
+    );
+  }
+
+  @override
+  void dispose() {
+    passwordController.dispose();
+    super.dispose();
+  }
+}
