7e0e5082eb0241b666aa7ab62fb7103472dda43f
yousefRafal <146264504+yousefRafal@users.noreply.github.com>
2024-07-23 21:19:50 +0300

    added password validator singUp & added singUpForm

---
+++ lib/features/sign_up/ui/widgets/password_validator_level.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+
+class BuildPasswordValidatorWidget extends StatelessWidget {
+  BuildPasswordValidatorWidget(
+      {super.key,
+      required this.hasLowerCase,
+      required this.hasUpperCase,
+      required this.hasSpecialCharacter,
+      required this.hasNumber,
+      // required this.value,
+      required this.values,
+      required this.hasMiningth});
+  final bool hasLowerCase;
+  final bool hasUpperCase;
+  final bool hasSpecialCharacter;
+  final bool hasNumber;
+  final bool hasMiningth;
+  // final List<bool> value;
+  final List<bool> values;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Flex(
+          direction: Axis.horizontal,
+          children: List.generate(values.length, (index) {
+            return Flexible(
+              flex: 1,
+              child: Container(
+                margin: const EdgeInsets.only(right: 3),
+                height: 2,
+                decoration: BoxDecoration(
+                  color: values[index] ? Colors.green : Colors.red,
+                  borderRadius: BorderRadius.circular(5),
+                ),
+              ),
+            );
+          }),
+        ),
+      ],
+    );
+  }
+}
